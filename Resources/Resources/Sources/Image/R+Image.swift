7526dddfa7d08a557e2775307df6c4d65344a774
JungMin Ahn <jungminahn.minsone@gmail.com>
2020-01-16 01:56:45 +0900

    modify

--- Resources/Resources/Sources/Image/R+Image.swift
+++ Resources/Resources/Sources/Image/R+Image.swift
@@ -9,19 +9,58 @@
 import Foundation
 import UIKit
 
-public extension R {
-    enum Image {}
+extension R {
+    public struct Image: _ExpressibleByImageLiteral {
+        let image: UIImage
+
+        public init(imageLiteralResourceName path: String) {
+            if let image = UIImage(named: path, in: R.bundle, compatibleWith: nil) {
+                self.image = image
+            } else {
+                assert(false, "해당 이미지가 없습니다.")
+                self.image = UIImage()
+            }
+        }
+
+        public static func name(_ image: Image) -> UIImage {
+            return image.image
+        }
+    }
 }
+
 public extension R.Image {
-    static var theme1: UIImage { .load(name: "theme1") }
-    static var theme2: UIImage { .load(name: "theme2") }
-    static var theme3: UIImage { .load(name: "theme3") }
-    static var theme4: UIImage { .load(name: "theme4") }
-    static var theme5: UIImage { .load(name: "theme5") }
-    static var theme6: UIImage { .load(name: "theme6") }
-    static var theme7: UIImage { .load(name: "theme7") }
-    static var theme8: UIImage { .load(name: "theme8") }
-    static var digits: UIImage { .load(name: "Digits") }
-    static var rotationLock: UIImage { .load(name: "rotation_lock") }
-    static var rotationUnLock: UIImage { .load(name: "rotation_unlock") }
+    static var theme1: UIImage { Self.name(#imageLiteral(resourceName: "theme1")) }
+    static var theme2: UIImage { Self.name(#imageLiteral(resourceName: "theme2")) }
+    static var theme3: UIImage { Self.name(#imageLiteral(resourceName: "theme3")) }
+    static var theme4: UIImage { Self.name(#imageLiteral(resourceName: "theme4")) }
+    static var theme5: UIImage { Self.name(#imageLiteral(resourceName: "theme5")) }
+    static var theme6: UIImage { Self.name(#imageLiteral(resourceName: "theme6")) }
+    static var theme7: UIImage { Self.name(#imageLiteral(resourceName: "theme7")) }
+    static var theme8: UIImage { Self.name(#imageLiteral(resourceName: "theme8")) }
+    static var digits: UIImage { Self.name(#imageLiteral(resourceName: "Digits")) }
+    static var rotationLock: UIImage { Self.name(#imageLiteral(resourceName: "rotation_lock")) }
+    static var rotationUnLock: UIImage { Self.name(#imageLiteral(resourceName: "rotation_unlock")) }
 }
+
+/*
+ extension UIImage {
+     static func image(_ rImage: R.Image) -> UIImage {
+         return rImage.image
+     }
+ }
+
+ public extension R.Image {
+     static var theme1: UIImage { .image(#imageLiteral(resourceName: "theme1")) }
+     static var theme2: UIImage { .image(#imageLiteral(resourceName: "theme2")) }
+     static var theme3: UIImage { .image(#imageLiteral(resourceName: "theme3")) }
+     static var theme4: UIImage { .image(#imageLiteral(resourceName: "theme4")) }
+     static var theme5: UIImage { .image(#imageLiteral(resourceName: "theme5")) }
+     static var theme6: UIImage { .image(#imageLiteral(resourceName: "theme6")) }
+     static var theme7: UIImage { .image(#imageLiteral(resourceName: "theme7")) }
+     static var theme8: UIImage { .image(#imageLiteral(resourceName: "theme8")) }
+     static var digits: UIImage { .image(#imageLiteral(resourceName: "Digits")) }
+     static var rotationLock: UIImage { .image(#imageLiteral(resourceName: "rotation_lock")) }
+     static var rotationUnLock: UIImage { .image(#imageLiteral(resourceName: "rotation_unlock")) }
+ }
+
+ */
